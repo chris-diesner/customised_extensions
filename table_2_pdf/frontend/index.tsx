b9ea0e749595b5b91aacb7f2e1cf64cb0671a3d5
Christian Diesner <chr.diesner@mailbox.org>
2025-12-12 15:37:38 +0200

    first commit

---
+++ table_2_pdf/frontend/index.tsx
@@ -0,0 +1,10 @@
+import {initializeBlock} from '@airtable/blocks/ui';
+import React from 'react';
+import './style.css';
+
+function HelloWorldTypescriptApp() {
+    // YOUR CODE GOES HERE
+    return <div>Hello world 🚀</div>;
+}
+
+initializeBlock(() => <HelloWorldTypescriptApp />);
